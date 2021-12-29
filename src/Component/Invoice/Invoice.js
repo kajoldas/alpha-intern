937c2a57c5fec5139410e8ad695b80bad8779241
kajoldas <kajol.nwu@gmail.com>
2021-12-29 13:44:51 +0600

    first commit

---
+++ src/Component/Invoice/Invoice.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Invoice = () => {
+    return (
+        <div>
+            <h1>Thank you,Mushfiq Rahman for your </h1>
+            <h1>purchase</h1>
+            
+        </div>
+    );
+};
+
+export default Invoice;
